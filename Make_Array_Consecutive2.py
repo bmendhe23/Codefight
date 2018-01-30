badbb7ac86d912bafa1361fa91572fd356911ca7
Bhushan Mendhe <bhushanmendhe15@gmail.com>
2018-01-30 12:15:07 +0530

    Update Make_Array_Consecutive2.py

--- Make_Array_Consecutive2.py
+++ Make_Array_Consecutive2.py
@@ -1,2 +1,9 @@
 def makeArrayConsecutive2(statues):
     return max(statues) - min(statues) - len(statues) + 1
+
+
+# another way more logical to do
+
+def makeArrayConsecutive2(statues):
+    new_array = list(range(min(statue),max(statues)+1))
+    return len(new_array)-len(statues)
